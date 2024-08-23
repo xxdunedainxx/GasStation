ee8b4f40619d87f38c8e1cffd7d55ff67a395842
xxdunedainxx <zrmmaster92@gmail.com>
2024-08-22 23:57:18 -0700

    NPC movement with movement agents!

--- Godot/UI/Sprites/NPC/NPC.gd
+++ Godot/UI/Sprites/NPC/NPC.gd
@@ -6,14 +6,30 @@ extends Node
 
 # Original character 2D ref 
 var npcReference: CharacterBody2D
+var npcNavAgentRef: NavigationAgent2D
 
 # Attributes provided to the generic NPC script 
 var npcAttributes: Dictionary 
 
+# Current NPC movement Specification
+# This dictionary expects:
+# 	- destination coordinates: x/y coordinates as a vector 
+#	- movement speed 
+var npcMovementSpecification: Dictionary 
+
 # Attaches parent NPC ref 
-func attachNpcReference(npcRef: CharacterBody2D, npcAttributesRef: Dictionary):
+func attachNpcReference(
+npcRef: CharacterBody2D, 
+npcAttributesRef: Dictionary, 
+navAgent: NavigationAgent2D):
 	npcReference = npcRef 
 	npcAttributes = npcAttributesRef
+	npcNavAgentRef = navAgent
+	
+	npcMovementSpecification = {
+		"npcDestination" : npcReference.global_position + Vector2(-30,5) 
+	}
+
 
 func handleEventSignal(eventData: Dictionary):
 	Logging.debugLog("NPC handle event", "NPC.gd") 	
@@ -24,6 +40,8 @@ func _ready():
 		Constants.GENERIC_EVENT_SYSTEM,
 		handleEventSignal
 	) 
+	
+		
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -41,8 +59,29 @@ func __getNPCSpeed() -> int:
 	return npcAttributes[Constants.NPC_SPEED]
 
 # Evaluate npc movement 
+# Here we use manhattan distance, so to do very linear x/y movements 
+# TODO - need to determine if objects are in the way, and adjust the vector 
 func __evalMoveNPC():
-	npcReference.velocity = Vector2.LEFT  * __getNPCSpeed()
-	npcReference.move_and_slide()
-	Sleep.sleep(get_tree(), .00000000001)
-	
+
+
+	# If we have a current, attached movement spec 
+	if not npcMovementSpecification.is_empty():
+
+		var npcDestination: Vector2 = npcMovementSpecification["npcDestination"]
+		npcNavAgentRef.target_position = npcDestination
+		
+		# TODO -- this could be moved into 'moveLeft', 'moveRight', etc.. methods 
+		#if npcDestination.x <= npcReference.position.x:
+			#npcReference.velocity = Vector2.LEFT  * __getNPCSpeed()
+		#elif npcDestination.x >= npcReference.position.x:
+			#npcReference.velocity = Vector2.RIGHT  * __getNPCSpeed()
+		#elif npcDestination.y <= npcReference.position.y:
+			#npcReference.velocity = Vector2.DOWN  * __getNPCSpeed()
+		#elif npcDestination.y >= npcReference.position.y:
+			#npcReference.velocity = Vector2.UP  * __getNPCSpeed()
+		
+		var directionToMove: Vector2 = npcNavAgentRef.get_next_path_position() - npcReference.global_position
+		directionToMove = directionToMove.normalized()
+		npcReference.velocity = directionToMove * __getNPCSpeed()
+		npcReference.move_and_slide()
+		Sleep.sleep(get_tree(), .00000000001)
--- Godot/UI/Sprites/NPC/OscarTheBoss/OscarTheBoss.gd
+++ Godot/UI/Sprites/NPC/OscarTheBoss/OscarTheBoss.gd
@@ -1,12 +1,17 @@
 extends CharacterBody2D
 
 @onready var npc = $Npc
+@onready var navAgent = $NavigationAgent2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	npc.attachNpcReference(self, {
-		Constants.NPC_SPEED : 10
-	})
+	npc.attachNpcReference(
+		self, 
+		{
+			Constants.NPC_SPEED : 10
+		},
+		navAgent
+	)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
